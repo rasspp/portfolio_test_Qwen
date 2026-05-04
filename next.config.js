d8277926fd8eb53f1b3299febc01059723b199c0
ridho8977 <ridho8977@gmail.com>
2026-05-04 03:09:48 +0000

    update readme file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    allowedDevOrigins: ['3000-firebase-portfolio-with-qwen-1777860457276.cluster-bqwaigqtxbeautecnatk4o6ynk.cloudworkstations.dev'],
+}
 
 module.exports = nextConfig
